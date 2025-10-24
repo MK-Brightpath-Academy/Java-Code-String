ab4e96d9c6494f1e4cf1298a4cedcbbeeb89394a
Mukesh Yadav <72705190+MK-Brightpath-Academy@users.noreply.github.com>
2025-10-24 19:48:03 +0530

    Time: 3 ms (59.19%) | Memory: 42.3 MB (15.36%) - LeetSync

---
+++ 20-valid-parentheses/valid-parentheses.java
@@ -0,0 +1,23 @@
+class Solution {
+    public boolean isValid(String s) {
+         Stack<Character> stack = new Stack<>();
+
+        for (char c : s.toCharArray()) {
+            if (c == '(' || c == '{' || c == '[') {
+                stack.push(c);
+            } else {
+                // If stack is empty or top does not match
+                if (stack.isEmpty()) return false;
+                char top = stack.pop();
+                if ((c == ')' && top != '(') ||
+                    (c == '}' && top != '{') ||
+                    (c == ']' && top != '[')) {
+                    return false;
+                }
+            }
+        }
+
+        // If stack is empty, all brackets are closed properly
+        return stack.isEmpty();
+    }
+}
